ce21c706b1fc4fae7a7c2ac9db905804a75fb7c2
frsargua <frsargua@gmail.com>
2022-06-21 16:38:24 +0100

    Employee class created

---
+++ src/lib/Employee.js
@@ -0,0 +1,22 @@
+class Employee {
+  constructor(name, email) {
+    this.name = name;
+    this.id = this.idGenerator;
+    this.email = email;
+  }
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  idGenerator() {
+    return Math.floor((1 + Math.random()) * 0x10000);
+  }
+}
