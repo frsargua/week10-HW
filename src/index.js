3d6432c93a69be604f84098dd822d85f92bbacee
frsargua <frsargua@gmail.com>
2022-06-23 14:24:06 +0100

    removed unneeded functions

--- src/index.js
+++ src/index.js
@@ -115,28 +115,6 @@ const enquirerFunction = () => {
 };
 
 //Render page
-const renderHtml = () => {};
-const renderHTMLhead = () => {
-  return `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
-        />
-        <script
-          src="https://kit.fontawesome.com/c6e9c752f1.js"
-          crossorigin="anonymous"
-        ></script>
-        <link rel="stylesheet" href="./assets/css/styles.css" />
-        <title>Team Profile</title>
-      </head>`;
-};
 const renderManager = (arrayStaff) => {
   let stringer = ``;
   console.log("Outer");
